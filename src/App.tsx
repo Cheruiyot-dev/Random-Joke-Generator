98897dc2a1afd1fa0530ab5e2f7c71909a13c3bc
Cheruiyot-dev <allankipkoech9@gmail.com>
2024-05-28 02:02:07 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        
+      <h1>Joke Generator</h1>
+      
+     
+    </>
+  )
+}
+
+export default App
