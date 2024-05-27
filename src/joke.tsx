edea0bd6160868e717b00992af9d6c98ee92f503
Cheruiyot-dev <allankipkoech9@gmail.com>
2024-05-28 02:46:18 +0300

    implement joke api fetch and pass fetchapi prop to button component

--- src/joke.tsx
+++ src/joke.tsx
@@ -1,9 +1,29 @@
 import Button from "./Button";
 import './joke.css'
-const Joke = () => {
+import { useState } from "react";
+
+interface JokeData {
+    joke: string;
+}
+
+const Joke: React.FC = () => {
+    const [Joke, setJoke] = useState<string>("");
+    const fetchApi = async () => {
+        try{
+            const response = await fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single");
+            const data: JokeData = await response.json()
+            setJoke(data.joke)
+        } catch(error) {
+            console.error("Error fetching joke:", error);
+            
+        }
+        
+      
+    };
     return(
         <div className="joke">
-            <Button/>
+            <Button callApi={fetchApi}/>
+            <p>{Joke}</p>
         </div>
     )
 }
